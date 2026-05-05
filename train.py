3fb2a73a8e4355725b5af68ead408118a263854b
lzz24 <lzz24@mails.tsinghua.edu.cn>
2026-05-06 04:53:12 +0800

    v3

--- train.py
+++ train.py
@@ -11,14 +11,18 @@ def main():
        results = model.train(
            data="./NEU-DET/data.yaml",
            epochs=50,                  # 训练轮数
-           imgsz=800,                  # 图像缩放尺寸
+           imgsz=640,                  # 图像缩放尺寸
            batch=16,                   # 每次吞吐的图片数量 (4060 8G 推荐值)
            device=0,                   # 调用第一张 GPU
            workers=2,                  # 限制数据加载线程，防止内存爆炸
-           optimizer='AdamW',
-           lr0=0.001,                  # 初始学习率
+           optimizer='auto',           # 自动选择优化器
+       
+           copy_paste=0.2,              
+           mosaic=0.5,                  
+           erasing=0.4,                 
+
            project="Defect_Results",   # 训练结果保存的主文件夹名
-           name="v2_train"             # 本次训练的子文件夹名
+           name="v3_train"             # 本次训练的子文件夹名
        )
 
        print(">>> 训练结束。")
