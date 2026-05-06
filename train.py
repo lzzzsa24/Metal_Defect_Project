99a0f9e5cdd70ee9af3e962e70bb3cb5a49d2d47
lzz24 <lzz24@mails.tsinghua.edu.cn>
2026-05-06 16:51:29 +0800

    v9

--- train.py
+++ train.py
@@ -10,19 +10,15 @@ def main():
        print(">>> 启动拟合...")
        results = model.train(
            data="./NEU-DET/data.yaml",
-           epochs=200,                  # 训练轮数
+           epochs=50,                  # 训练轮数
            imgsz=800,                  # 图像缩放尺寸
-           batch=16,                   # 每次吞吐的图片数量 (4060 8G 推荐值)
+           batch=32,                   # 每次吞吐的图片数量
            device=0,                   # 调用第一张 GPU
            workers=2,                  # 限制数据加载线程，防止内存爆炸
            optimizer='auto',           # 自动选择优化器
        
-           copy_paste=0.2,              
-           mosaic=0.5,                  
-           erasing=0.4,                 
-
            project="Defect_Results",   # 训练结果保存的主文件夹名
-           name="v8_train"             # 本次训练的子文件夹名
+           name="v9_train"             # 本次训练的子文件夹名
        )
 
        print(">>> 训练结束。")
